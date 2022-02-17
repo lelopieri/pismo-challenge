2277d308acc6b15a33c29f5e2c322eaf13a90914
Luiz Rogerio De Pieri <lelo.pieri@gmail.com>
2022-02-16 22:19:24 -0300

    Project Creation using start.spring.io

---
+++ src/main/java/br/com/pieri/pismotechnicalchallenge/PismoTechnicalChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.pieri.pismotechnicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoTechnicalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PismoTechnicalChallengeApplication.class, args);
+	}
+
+}
